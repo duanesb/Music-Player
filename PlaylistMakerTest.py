3c2d8535a3727220dc20246a806f9d8b197e4005
LuiETR <luisernestorres07@gmail.com>
2025-02-26 14:06:36 -0400

    name and button creation for playlist

--- PlaylistMakerTest.py
+++ PlaylistMakerTest.py
@@ -8,3 +8,12 @@ class Playlist:
     def add_song(self, file_name):
         self.songs.append(file_name)
 
+def main(page: ft.Page):
+    page.title = "Playlist Manager"
+    
+    playlists = {}
+
+    playlist_name_input = ft.TextField(label="Enter Playlist Name", width=300)
+    create_button = ft.ElevatedButton("Create Playlist", on_click=lambda e: create_playlist(e, playlist_name_input.value))
+    
+    
