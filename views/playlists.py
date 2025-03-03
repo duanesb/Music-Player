3b216eb654bbc492b21bcc64a12075583bde0df6
duanesb <duanebollig.school@gmail.com>
2025-03-03 10:12:17 -0400

    almost done, missing actions

--- views/playlists.py
+++ views/playlists.py
@@ -8,7 +8,7 @@ def PlaylistsContent():
     information = [
         {
             "name": folder,
-            "image": os.listdir(os.path.join(playlistPath,folder,"metadata"))[0],
+            "img": os.listdir(os.path.join(playlistPath,folder,"metadata"))[0],
             "songs": [
                 song for song in os.listdir(os.path.join(playlistPath, folder))
                 if song.endswith(".mp3")
@@ -18,56 +18,50 @@ def PlaylistsContent():
         if os.path.isdir(os.path.join(playlistPath, folder))
     ]
 
-    playlistContainer = ft.Container(width=appWidth,height=appHeight-190)
-
-    # playlistContainer = ft.Container(width=appWidth,height=appHeight-190,
-    #     content=ft.Row(
-    #         controls=[
-    #             ft.Container(
-    #                 content=ft.Row(
-    #                     controls=[
-    #                         ft.Container(
-    #                             width=appWidth/3.5,height=appWidth/3.5+30,bgcolor="#2d2e33",border_radius=ft.border_radius.vertical(top=5),
-    #                             content=ft.Column(
-    #                                 controls=[
-    #                                     ft.Text(i["name"],size=14,width=appWidth/3.5,text_align=ft.TextAlign.CENTER),
-    #                                     ft.Image(src=i["src"],width=appWidth/3.5,height=appWidth/3.5),
-    #                                 ],
-    #                                 spacing=4,
-    #                                 alignment=ft.MainAxisAlignment.END
-    #                             )
-    #                         ),
-    #                         ft.DataTable(height=appWidth/3.5+30,
-    #                             columns=[
-    #                                 ft.DataColumn(ft.Text("Song Name")),
-    #                                 ft.DataColumn(ft.Text("Action"))
-    #                             ],
-    #                             rows=[
-    #                                 ft.DataRow(
-    #                                     cells=[
-    #                                         ft.DataCell(ft.Text("yes")),
-    #                                         ft.DataCell(ElevatedButton("Remove",100,None))
-    #                                     ]
-    #                                 )
-    #                             ]
-    #                         )
-    #                     ]
-    #                 ),
-    #             ) for i in playlists
-    #         ],
-    #         wrap=True,
-    #         spacing=24,
-    #         run_spacing=24
-    #     ),
-    #     alignment=ft.alignment.top_left
-    # )
+    playlistContainer = ft.Column(width=appWidth,height=appHeight-190,
+        controls=[
+            ft.Container( # PLAYLIST
+                content = ft.Row(
+                    controls= [
+                        ft.Container( # PLAYLIST NAME AND IMAGE
+                            width=appWidth/3.5, height=appWidth/3.5+30,bgcolor="#2d2e33",
+                            border_radius=ft.border_radius.vertical(top=5),
+                            content=ft.Column(
+                                controls=[
+                                    ft.Text(playlist["name"],size=14,width=appWidth/3.5,text_align=ft.TextAlign.CENTER),
+                                    ft.Image(playlist["img"],width=appWidth/3.5,height=appWidth/3.5)
+                                ],
+                                spacing=4,
+                                alignment=ft.MainAxisAlignment.END
+                            )
+                        ),
+                        ft.DataTable( # SONG AND ACTIONS
+                            columns=[
+                                ft.DataColumn(ft.Text("Song Name")),
+                                ft.DataColumn(ft.Text("Action"))
+                            ],
+                            rows=[
+                                ft.DataRow(
+                                    cells=[
+                                        ft.DataCell(ft.Text(song[:-4],size=10)),
+                                        ft.DataCell(ft.ElevatedButton("Remove",height=15))
+                                    ]
+                                )  for song in playlist["songs"]
+                            ],
+                            column_spacing=0
+                        )
+                    ]
+                )
+            ) for playlist in information
+        ],
+        scroll=True
+    )
     content = ft.Container(width=appWidth,height=appHeight,
         content = ft.Column(
             controls=[
                 ft.Text("View Playlists",size=30,weight="bold"),
                 ft.Divider(thickness=2,height=4),
-                # playlistContainer,
-                # ElevatedButton("Upload Playlist",150,None),
+                playlistContainer,
                 ft.Divider(thickness=2,height=4),
                 NavButton("Return Home",lambda _:_.page.go("/home"),appWidth)
             ]
