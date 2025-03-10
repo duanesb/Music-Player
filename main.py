16e68f088eaf5656926e7eac7b024529479b7300
Heesung Ahn <167945999+HeesungA@users.noreply.github.com>
2025-03-10 14:00:53 -0400

    Revert "Fixes"
    This reverts commit 48fed34b3ca133d7475156d3e777a0e3aea1f4e2.

--- main.py
+++ main.py
@@ -1,17 +1,84 @@
 import flet as ft
-import flet_audio as fta
 from objects import appWidth,appHeight,baseColor,View
 from views.home import HomeContent
-from views.playlists import PlaylistsContent
-from views.playlists import PlaylistsContent, globalAudio
+<<<<<<< HEAD
+from views.playlists import PlaylistsContent, globalAudio #! Included the audio object created in playlist view to add it to the page overlay
 from views.songViewer import SongViewerContent
 import os
 import certifi
-@@ -27,6 +28,7 @@ def main(page: ft.Page):
+os.environ['SSL_CERT_FILE'] = certifi.where()
+def filePickerResult(e:ft.FilePickerResultEvent):
+    if e.files:
+        path = e.files[0].path
+def handleSongPickerResult(e):
+    if e.files:
+        paths = [file.path for file in e.files]
+=======
+from views.playlists import PlaylistsContent
+import os
+import certifi
+
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+def filePickerResult(e:ft.FilePickerResultEvent):
+    if e.files:
+        path = e.files[0].path
+
+def handleSongPickerResult(e):
+    if e.files:
+        paths = [file.path for file in e.files]
+
+>>>>>>> parent of 48fed34 (Fixes)
+def main(page: ft.Page):
+    # CONFIG
+    page.title = "Music Player"
+    page.window.width = appWidth
+    page.window.height = appHeight
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 48fed34 (Fixes)
+    # FILE PICKER
+    filePicker = ft.FilePicker(on_result=filePickerResult)
     songFilePicker = ft.FilePicker(on_result=handleSongPickerResult)
     page.overlay.append(filePicker)
     page.overlay.append(songFilePicker)
+<<<<<<< HEAD
     page.overlay.append(globalAudio) #! Adding playlist audio to the overlay
+=======
 
+>>>>>>> parent of 48fed34 (Fixes)
     # ROUTING
     def route_change(route):
+        page.views.clear()
+        match(page.route):
+<<<<<<< HEAD
+            case "/songViewer":
+                View("/songViewer",SongViewerContent()).set(page)
+=======
+>>>>>>> parent of 48fed34 (Fixes)
+            case "/playlists":
+                View("/playlists",PlaylistsContent()).set(page)
+            case _:
+                View("/home",HomeContent(filePicker,songFilePicker)).set(page)
+        
+        page.update()
+    
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view)
+    
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+<<<<<<< HEAD
+    # STARTING PAGE
+    page.go("/home")
+ft.app(target=main,assets_dir="assets")
+=======
+
+    # STARTING PAGE
+    page.go("/home")
+
+ft.app(target=main,assets_dir="assets")
+>>>>>>> parent of 48fed34 (Fixes)
--- views/playlists.py
+++ views/playlists.py
@@ -1,25 +1,60 @@
 import flet as ft
-import flet_audio as fta #! You need to install the official audio handling module for this, the default one will be deprecated in the future
 import os
-import threading
-import time
-from pygame import mixer
+<<<<<<< HEAD
 # import threading
 # import time
 # from pygame import mixer #! Not necessary anymore
 from objects import appWidth, appHeight, NavButton, ElevatedButton, ActionButton
-
-mixer.init()
 # mixer.init()
 globalAudio = fta.Audio(src="playlistBank/TestPlay/All at Once.mp3", autoplay=False) #! Create a global audio object to replace the mixer
+=======
+import threading
+import time
+from pygame import mixer
+from objects import appWidth, appHeight, NavButton, ElevatedButton, ActionButton
+
+mixer.init()
+>>>>>>> parent of 48fed34 (Fixes)
 current_index = 0
-
 def PlaySong(song_list):
-@@ -15,40 +17,52 @@ def PlaySong(song_list):
+    global current_index
+    # Reset to the beginning if we've reached the end
+    if current_index >= len(song_list):
         current_index = 0
-
     song_path = song_list[current_index]
+<<<<<<< HEAD
     print(song_path)
+    # def monitor_song():
+    #     global current_index
+    #     # Wait until the current song finishes
+    #     while globalAudio.get_current_position() < globalAudio.get_duration():
+    #         time.sleep(0.5)
+    #     # Increment and reset the index if necessary
+    #     current_index += 1
+    #     if current_index >= len(song_list):
+    #         current_index = 0  # Loop back to first song
+    #     PlaySong(song_list)
+    globalAudio.src = song_path #! Changing song path based on your algorightm
+    globalAudio.update()
+    globalAudio.play() #! Play the song, duh
+    # mixer.music.load(song_path)
+    # mixer.music.set_volume(1)
+    # mixer.music.play()
+    # threading.Thread(target=monitor_song, daemon=True).start()
+
+def PauseSong():
+    # mixer.music.pause()
+    globalAudio.pause() #! Mixer replacement
+
+def UnpauseSong():
+    # mixer.music.unpause()
+    globalAudio.resume() #! Mixer replacement
+
+def StopSong():
+    # mixer.music.stop()
+    globalAudio.seek(0)
+    globalAudio.pause() #! Mixer replacement
+=======
 
     def monitor_song():
         global current_index
@@ -31,46 +66,22 @@ def PlaySong(song_list):
         if current_index >= len(song_list):
             current_index = 0  # Loop back to first song
         PlaySong(song_list)
-    # def monitor_song():
-    #     global current_index
-    #     # Wait until the current song finishes
-    #     while globalAudio.get_current_position() < globalAudio.get_duration():
-    #         time.sleep(0.5)
-    #     # Increment and reset the index if necessary
-    #     current_index += 1
-    #     if current_index >= len(song_list):
-    #         current_index = 0  # Loop back to first song
-    #     PlaySong(song_list)
 
     mixer.music.load(song_path)
     mixer.music.set_volume(1)
     mixer.music.play()
-    globalAudio.src = song_path #! Changing song path based on your algorightm
-    globalAudio.update()
-    globalAudio.play() #! Play the song, duh
 
     threading.Thread(target=monitor_song, daemon=True).start()
-    # mixer.music.load(song_path)
-    # mixer.music.set_volume(1)
-    # mixer.music.play()
-
-    # threading.Thread(target=monitor_song, daemon=True).start()
 
 def PauseSong():
     mixer.music.pause()
-    # mixer.music.pause()
-    globalAudio.pause() 
 
 def UnpauseSong():
     mixer.music.unpause()
-    # mixer.music.unpause()
-    globalAudio.resume()
 
 def StopSong():
     mixer.music.stop()
-    # mixer.music.stop()
-    globalAudio.seek(0)
-    globalAudio.pause()
+>>>>>>> parent of 48fed34 (Fixes)
 
 def SkipSong(song_list):
     global current_index
@@ -78,22 +89,111 @@ def SkipSong(song_list):
     current_index += 1
     if current_index >= len(song_list):
         current_index = 0  # Loop back to the first song
-    mixer.music.stop()
+<<<<<<< HEAD
     # mixer.music.stop()
     globalAudio.seek(0)
-    globalAudio.pause()
+    globalAudio.pause() #! Mixer replacement
 
     PlaySong(song_list)
     print("Current index:", current_index)
+=======
+    mixer.music.stop()
+    PlaySong(song_list)
+    print("Current index:", current_index)
 
-@@ -130,8 +144,8 @@ def PlaylistsContent():
+>>>>>>> parent of 48fed34 (Fixes)
+def PlaylistsContent():
+    playlistPath = "assets/playlistBank"
+    information = [
+        {
+            "name": folder,
+            "img": os.path.abspath(os.path.join(playlistPath, folder, "metadata",
+                                                   os.listdir(os.path.join(playlistPath, folder, "metadata"))[0]))
+                   if os.listdir(os.path.join(playlistPath, folder, "metadata")) else "",
+            "songs": [
+                os.path.join(playlistPath, folder, song)
+                for song in os.listdir(os.path.join(playlistPath, folder))
+                if song.endswith(".mp3")
+            ]
+        }
+        for folder in os.listdir(playlistPath)
+        if os.path.isdir(os.path.join(playlistPath, folder))
+    ]
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 48fed34 (Fixes)
+    playlistContainer = ft.Column(
+        width=appWidth, height=appHeight - 190,
+        controls=[
+            ft.Container(  # PLAYLIST
+                content=ft.Row(
+                    controls=[
+                        ft.Container(  # PLAYLIST NAME AND IMAGE
+                            width=appWidth / 3.5, height=appWidth / 3.5 + 30, bgcolor="#2d2e33",
+                            border_radius=ft.border_radius.vertical(top=5),
+                            content=ft.Column(
+                                controls=[
+                                    ft.Text(playlist["name"], size=14, width=appWidth / 3.5,
+                                            text_align=ft.TextAlign.CENTER),
+                                    ft.Image(playlist["img"], width=appWidth / 3.5,
+                                             height=appWidth / 3.5) if playlist["img"] else None
+                                ],
+                                spacing=4,
+                                alignment=ft.MainAxisAlignment.END
+                            )
+                        ),
+                        ft.DataTable(  # SONG LIST
+                            columns=[
+                                ft.DataColumn(ft.Text("Song Name")),
+                                ft.DataColumn(ft.Text("Action"))
+                            ],
+                            rows=[
+                                ft.DataRow(
+                                    cells=[
+                                        ft.DataCell(ft.Text(os.path.basename(song)[:-4], size=10)),
+                                        ft.DataCell(ft.ElevatedButton("Remove", height=20))
+                                    ]
+                                ) for song in playlist["songs"]
+                            ],
+                            column_spacing=0, width=200
+                        ),
+                        ft.Column(  # PLAY, PAUSE, UNPAUSE, STOP, AND SKIP BUTTONS
+                            controls=[
+                                ElevatedButton("Play", 100, lambda _, songs=playlist["songs"]: PlaySong(songs)),
+                                ElevatedButton("Pause", 100, lambda _: PauseSong()),
+                                ElevatedButton("Unpause", 100, lambda _: UnpauseSong()),
+                                ElevatedButton("Stop", 100, lambda _: StopSong()),
+                                ElevatedButton("Skip", 100, lambda _, songs=playlist["songs"]: SkipSong(songs))
+                            ]
+                        )
+                    ]
+                )
+            )
+            for playlist in information
+        ],
+        scroll=True
+    )
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 48fed34 (Fixes)
+    content = ft.Container(
+        width=appWidth, height=appHeight,
+        content=ft.Column(
+            controls=[
+                ft.Text("View Playlists", size=30, weight="bold"),
                 ft.Divider(thickness=2, height=4),
                 playlistContainer,
                 ft.Divider(thickness=2, height=4),
-                NavButton("Return Home", lambda _: _.page.go("/home"), appWidth)
+<<<<<<< HEAD
                 NavButton("Return Home", lambda _: _.page.go("/home"), appWidth),
             ]
-        )
         ),
+=======
+                NavButton("Return Home", lambda _: _.page.go("/home"), appWidth)
+            ]
+        )
+>>>>>>> parent of 48fed34 (Fixes)
     )
     return content
